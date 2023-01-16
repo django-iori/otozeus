8bb3ddc21b2551f5078047129cfef63d3b78f772
django-iori <iori4321999@gmail.com>
2023-01-16 15:22:38 +0900

    install ffmpeg

--- otozeus/urls.py
+++ otozeus/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('otozeusapp/', include('otozeusapp.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- otozeusapp/models.py
+++ otozeusapp/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 
 class Demo(models.Model):
-    video = models.FileField(upload_to='uploads/', max_length=255)
+    video = models.FileField(upload_to='uploads/')
