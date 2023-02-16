3898c6d2d16bcfba79dc68549cfeb93eb81a2ac4
django-iori <iori4321999@gmail.com>
2023-02-16 16:25:49 +0900

    Delete DemoModel

--- otozeusapp/urls.py
+++ otozeusapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('demo/', DemoView.as_view()),
+    path('upload_from_ios/', UploadFromIOS.as_view()),
     path('delete/', DeleteView.as_view()),
     path('templates/', index_template, name='index_template'),
     path('test/', test, name='test'),
